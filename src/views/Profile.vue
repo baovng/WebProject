4b481bf13048871635e030e39649191d816b6e02
edwardrand <52064142+tanmay-kali@users.noreply.github.com>
2022-04-19 22:10:22 -0500

    Profile Fix

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -13,46 +13,13 @@
             <div class="row">
               <div class="col-xl-4">
                 <!-- Profile picture card-->
-                <div class="card mb-4 mb-xl-0">
-                  <div class="card-header">Profile Picture</div>
-                  <div class="card-body text-center">
-                    <!-- Profile picture image-->
-                    <img
-                      class="img-account-profile rounded-circle mb-2"
-                      src="assets/img/illustrations/profiles/profile-1.png"
-                      alt=""
-                    />
-                    <!-- Profile picture help block-->
-                    <div class="small font-italic text-muted mb-4">
-                      JPG or PNG no larger than 5 MB
-                    </div>
-                    <!-- Profile picture upload button-->
-                    <button class="btn btn-primary" type="button">
-                      Upload new image
-                    </button>
-                  </div>
-                </div>
               </div>
-              <div class="col-xl-8">
+              <div class="col-xl-12">
                 <!-- Account details card-->
                 <div class="card mb-4">
                   <div class="card-header">Account Details</div>
                   <div class="card-body">
                     <form>
-                      <!-- Form Group (username)-->
-                      <div class="mb-3">
-                        <label class="small mb-1" for="inputUsername"
-                          >Username (how your name will appear to other users on
-                          the site)</label
-                        >
-                        <input
-                          class="form-control"
-                          id="inputUsername"
-                          type="text"
-                          placeholder="Enter your username"
-                          value="username"
-                        />
-                      </div>
                       <!-- Form Row-->
                       <div class="row gx-3 mb-3">
                         <!-- Form Group (first name)-->
@@ -87,14 +54,14 @@
                         <!-- Form Group (organization name)-->
                         <div class="col-md-6">
                           <label class="small mb-1" for="inputOrgName"
-                            >Organization name</label
+                            >Phone Number</label
                           >
                           <input
                             class="form-control"
                             id="inputOrgName"
-                            type="text"
+                            type="phone"
                             placeholder="Enter your organization name"
-                            value="Start Bootstrap"
+                            value="(857)763-684X"
                           />
                         </div>
                         <!-- Form Group (location)-->
@@ -128,29 +95,29 @@
                       <div class="row gx-3 mb-3">
                         <!-- Form Group (phone number)-->
                         <div class="col-md-6">
-                          <label class="small mb-1" for="inputPhone"
-                            >Phone number</label
+                          <label class="small mb-1" for="Password"
+                            >Password</label
                           >
                           <input
                             class="form-control"
-                            id="inputPhone"
+                            id="inputPassword"
                             type="tel"
-                            placeholder="Enter your phone number"
-                            value="555-123-4567"
+                            placeholder="************"
+                            value=""
                           />
                         </div>
                         <!-- Form Group (birthday)-->
                         <div class="col-md-6">
                           <label class="small mb-1" for="inputBirthday"
-                            >Birthday</label
+                            >Confirm Password</label
                           >
                           <input
                             class="form-control"
-                            id="inputBirthday"
+                            id="confirmPass"
                             type="text"
-                            name="birthday"
-                            placeholder="Enter your birthday"
-                            value="06/10/1988"
+                            name="confirmPass"
+                            placeholder="*************"
+                            value=""
                           />
                         </div>
                       </div>
